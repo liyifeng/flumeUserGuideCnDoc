75937e878895eadfd7c17847f20ea028099f31fd
LiEF <id@liyifeng.org>
2019-07-29 10:17:03 +0800

    增加了一个统计方式

--- _static/stat.js
+++ _static/stat.js
@@ -5,3 +5,5 @@
 
   gtag('config', 'UA-132785326-2'); 
 /** google end */
+
+<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1277865825'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "v1.cnzz.com/z_stat.php%3Fid%3D1277865825' type='text/javascript'%3E%3C/script%3E"));</script>
