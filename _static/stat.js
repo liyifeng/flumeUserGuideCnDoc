9a5f44d02ac1dab24631d25e5bb8ed25f6deb37e
LiEF <id@liyifeng.org>
2019-08-28 17:48:44 +0800

    add update tip

--- _static/stat.js
+++ _static/stat.js
@@ -1,3 +1,5 @@
+var myVersion = 12 ;
+
 /** google begin */
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
@@ -6,4 +8,3 @@
   gtag('config', 'UA-132785326-2'); 
 /** google end */
 
-<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1277865825'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "v1.cnzz.com/z_stat.php%3Fid%3D1277865825' type='text/javascript'%3E%3C/script%3E"));</script>
